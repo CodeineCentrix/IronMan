8330abf0cc6590ef85f468268d19c5fc5bf1a6d3
HaichH <healing.legodi@gmail.com>
2018-07-21 19:15:25 +0200

    Everything is working on my side, just animations and decorations are left

--- app/src/main/java/com/example/s216127904/codecentrix/GroupDetails.java
+++ app/src/main/java/com/example/s216127904/codecentrix/GroupDetails.java
@@ -9,5 +9,11 @@ public class GroupDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_details);
+        try {
+            getSupportActionBar().setTitle("Group Details");
+        }catch ( Exception a  ){
+
+        }
+
     }
 }
